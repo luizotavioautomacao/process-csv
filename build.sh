74ea68f34a4b4ea3f9e5c2494159983e9fb5369e
Luiz Otávio <luizotavioautomacao@gmail.com>
2024-07-13 00:11:45 -0300

    chore: commands to compile the .so library

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
-#!/bin/sh
-
+#!/bin/sh 
+## Conter os comandos necessários para compilar a biblioteca (.so) e o executável test_libcsv ##
+gcc -fPIC -c libcsv.c -o libcsv.o
+gcc -shared -o libcsv.so libcsv.o
+# gcc -o test_libcsv test_libcsv.c -L. -lcsv # (test_libcsv já está compilado)
