c67ac812eb51b9e0d71bfce4f5892eef2db9840a
Luiz Otávio <luizotavioautomacao@gmail.com>
2024-07-14 05:09:43 -0300

    chore: add remove-quotes in .so at build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/sh 
-## Conter os comandos necessários para compilar a biblioteca (.so) e o executável test_libcsv ##
+# Contain the commands necessary to compile the library (.so) and the test_libcsv executable
+
+# Compile remove-quotes.c to a shared object
+gcc -fPIC -c src/helpers/remove-quotes.c -o remove-quotes.o
+
+# Compile libcsv.c to a shared object
 gcc -fPIC -c libcsv.c -o libcsv.o
-gcc -shared -o libcsv.so libcsv.o
-# gcc -o test_libcsv test_libcsv.c -L. -lcsv # (test_libcsv já está compilado)
+
+# Generate a shared library (libcsv.so)
+gcc -shared -o libcsv.so libcsv.o remove-quotes.o
+
+# gcc -o test_libcsv test_libcsv.c -L. -lcsv # (test_libcsv is already compiled)
--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
 clear
+
+# Compile remove-quotes.c to an object file
+gcc -fPIC -c helpers/remove-quotes.c -o remove-quotes.o
+
+# Compile libcsv.c to an object file
 gcc -fPIC -c ../libcsv.c -o libcsv.o
-gcc -shared -o libcsv.so libcsv.o
-gcc -o main main.c
+
+# Generate a shared library (libcsv.so) including remove-quotes.o
+gcc -shared -o libcsv.so libcsv.o remove-quotes.o
+
+# Create the /bin directory if it doesn't exist
+mkdir -p bin
+
+# Move compiled files to the /bin directory
+mv libcsv.so bin/
+mv main bin/
+
+# Clean up object files
+rm libcsv.o remove-quotes.o
+
+# Change directory to /bin
+cd bin || exit
+
+# Run the program
 ./main
