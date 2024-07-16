256209f47a61eb0212c5c73ea860394ebaeb953e
Luiz Otávio <luizotavioautomacao@gmail.com>
2024-07-15 23:26:36 -0300

    fix: removing free objects

--- src/helpers/builder-free-csv.c
+++ src/helpers/builder-free-csv.c
@@ -4,10 +4,10 @@
 
 void freeCsvObjects(CsvLines csvLines, CsvHeader csvHeader, CsvFilter csvFilter, CsvSelection csvSelection)
 {
-    freeStringArray(csvHeader.headers);
-    freeStringArray(csvFilter.filter_operators);
-    free(csvSelection.selected_indice_column);
-    free(csvFilter.filter_indice_column);
-    // freeStringArray(lines);
-    // freeStringArray(filter_values); // error in big matrix ? free(): invalid pointer \n Aborted (core dumped)
+    // freeStringArray(csvHeader.headers);
+    // // freeStringArray(csvFilter.filter_operators);
+    // free(csvSelection.selected_indice_column);
+    // free(csvFilter.filter_indice_column);
+    // // freeStringArray(lines);
+    // // freeStringArray(filter_values); // error in big matrix ? free(): invalid pointer \n Aborted (core dumped)
 }
