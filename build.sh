e4aeda3c46f435fa33e0797fc04eb8e4ffc97b0e
Luiz Otávio <luizotavioautomacao@gmail.com>
2024-07-15 23:27:26 -0300

    chore: puting logs to build.sh

--- build.sh
+++ build.sh
@@ -1,52 +1,58 @@
 #!/bin/sh 
 # Contain the commands necessary to compile the library (.so) and the test_libcsv executable
 
-# Compile remove-quotes.c to a shared object
+echo "Compile remove-quotes.c to a shared object"
 gcc -fPIC -c src/helpers/remove-quotes.c -o remove-quotes.o
 
-# Compile split.c to a shared object
+echo "Compile split.c to a shared object"
 gcc -fPIC -c src/helpers/split.c -o split.o
 
-# Compile is-array-duplicate.c to a shared object
+echo "Compile is-array-duplicate.c to a shared object"
 gcc -fPIC -c src/helpers/is-array-duplicate.c -o is-array-duplicate.o
 
-# Compile free-string-array.c to a shared object
+echo "Compile free-string-array.c to a shared object"
 gcc -fPIC -c src/helpers/free-string-array.c -o free-string-array.o
 
-# Compile handle-error.c to a shared object
+echo "Compile handle-error.c to a shared object"
 gcc -fPIC -c src/helpers/handle-error.c -o handle-error.o
 
-# Compile read-file-contents.c to a shared object
+echo "Compile read-file-contents.c to a shared object"
 gcc -fPIC -c src/helpers/read-file-contents.c -o read-file-contents.o
 
-# Compile process-csv-lines.c to a shared object
+echo "Compile process-csv-lines.c to a shared object"
 gcc -fPIC -c src/helpers/process-csv-lines.c -o process-csv-lines.o
 
-# Compile process-csv-headers.c to a shared object
+echo "Compile process-csv-headers.c to a shared object"
 gcc -fPIC -c src/helpers/process-csv-headers.c -o process-csv-headers.o
 
-# Compile process-csv-selected.c to a shared object
+echo "Compile process-csv-selected.c to a shared object"
 gcc -fPIC -c src/helpers/process-csv-selected.c -o process-csv-selected.o
 
-# Compile process-csv-filters.c to a shared object
+echo "Compile process-csv-filters.c to a shared object"
 gcc -fPIC -c src/helpers/process-csv-filters.c -o process-csv-filters.o
 
-# Compile output-csv-headers.c to a shared object
+echo "Compile output-csv-headers.c to a shared object"
 gcc -fPIC -c src/helpers/output-csv-headers.c -o output-csv-headers.o
 
-# Compile output-csv-values.c to a shared object
+echo "Compile output-csv-values.c to a shared object"
 gcc -fPIC -c src/helpers/output-csv-values.c -o output-csv-values.o
 
-# Compile builder-free-csv.c to a shared object
+echo "Compile builder-free-csv.c to a shared object"
 gcc -fPIC -c src/helpers/builder-free-csv.c -o builder-free-csv.o
 
-# Compile libcsv.c to a shared object
+echo "Compile libcsv.c to a shared object"
 gcc -fPIC -c libcsv.c -o libcsv.o
 
-# Generate a shared library (libcsv.so)
-gcc -shared -o libcsv.so libcsv.o remove-quotes.o split.o is-array-duplicate.o free-string-array.o handle-error.o read-file-contents.o process-csv-lines.o process-csv-headers.o process-csv-selected.o process-csv-filters.o output-headers.o output-csv-headers.o output-csv-values.o builder-free-csv.o
+echo "Generate a shared library (libcsv.so)"
+gcc -shared -o libcsv.so libcsv.o remove-quotes.o split.o is-array-duplicate.o free-string-array.o handle-error.o read-file-contents.o process-csv-lines.o process-csv-headers.o process-csv-selected.o process-csv-filters.o output-csv-headers.o output-csv-values.o builder-free-csv.o
 
 # gcc -o test_libcsv test_libcsv.c -L. -lcsv # (test_libcsv is already compiled)
 
-# Clean up object files
-rm libcsv.o remove-quotes.o split.o is-array-duplicate.o free-string-array.o handle-error.o read-file-contents.o process-csv-lines.o process-csv-headers.o process-csv-selected.o process-csv-filters.o output-headers.o output-csv-headers.o output-csv-values.o builder-free-csv.o
+echo "Clean up object files"
+rm libcsv.o remove-quotes.o split.o is-array-duplicate.o free-string-array.o handle-error.o read-file-contents.o process-csv-lines.o process-csv-headers.o process-csv-selected.o process-csv-filters.o output-csv-headers.o output-csv-values.o builder-free-csv.o
+
+echo "Completing the build.sh ..."
+echo ""
+
+cp /app/test_libcsv /app/test_libcsv
+chmod +x /app/test_libcsv
